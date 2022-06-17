91db9ccb3e920d32d9a1f7988c70b65de558ed30
Dashstrom <incerti.felix@gmail.com>
2022-06-17 22:57:01 +0200

    funny list

--- funnyserver/__init__.py
+++ funnyserver/__init__.py
@@ -1,4 +1,13 @@
-__all__ = []  # type: ignore
+from .server import FileServer
+from .response import (
+    FileResponse, Response, ACCEPT, DENY, UPLOAD, DOWNLOAD, LIST
+)
+
+
+__all__ = [
+    "FileServer", "FileResponse", "Response",
+    "ACCEPT", "DENY", "UPLOAD", "DOWNLOAD", "LIST"
+]
 
 __version__ = "0.0.1"
 __author__ = "Dashstrom & William Madié"
--- funnyserver/server.py
+++ funnyserver/server.py
@@ -1,16 +1,17 @@
 import os
 import socket
 from struct import unpack
-
+from string import ascii_letters, digits
 from _thread import start_new_thread
 
-from response import (
+from .response import (
     FileResponse, Response, ACCEPT, DENY, UPLOAD, DOWNLOAD, LIST
 )
 
 HOST = '127.0.0.1'
 PORT = 9999
 STORAGE_DIR = os.path.join(os.path.dirname(__file__), 'storage')
+ALPHA = ascii_letters + digits + "_.-"
 
 
 class FileServer:
@@ -40,39 +41,37 @@ class FileServer:
 
     def download_file(self, filename: str) -> bytes:
         fpath = os.path.join(STORAGE_DIR, filename)
-        if os.path.exists(fpath):
+        if not os.path.exists(fpath):
+            res = Response(DENY)
+        else:
             with open(fpath, 'rb') as f:
                 res = FileResponse(f.read())
-        else:
-            res = Response(DENY)
         return res.pack()
-    
+
     def list_files(self, pattern: str) -> bytes:
         stored_files = os.listdir(STORAGE_DIR)
         matching_files = []
         for file in stored_files:
             if file.startswith(pattern):
                 matching_files.append(file)
-        res = FileResponse("\n".join(matching_files))
+        res = FileResponse("\n".join(matching_files).encode())
         return res.pack()
 
 
 def file_thread(conn: socket.socket, fs: FileServer) -> None:
     code = conn.recv(1)[0]   # Reads 1 byte
-    str_len = unpack('>I', conn.recv(4))[0]
+    str_len: int = unpack('>I', conn.recv(4))[0]  # type: ignore
     filename = conn.recv(str_len).decode('utf8')
+    data = Response(DENY).pack()
     print(code, str_len, filename)
-
-    if code == UPLOAD:
-        size = unpack('>I', conn.recv(4))[0]
-        file = conn.recv(size)
-        print(size, file)
-        data = fs.upload_file(filename, file)
-    elif code == DOWNLOAD:
-        data = fs.download_file(filename)
-    elif code == LIST:
-        data = fs.list_files(filename)
-    else:
-        data = Response(DENY).pack()
+    if all(c in ALPHA for c in filename):
+        if code == UPLOAD:
+            size: int = unpack('>I', conn.recv(4))[0]  # type: ignore
+            file = conn.recv(size)
+            data = fs.upload_file(filename, file)
+        elif code == DOWNLOAD:
+            data = fs.download_file(filename)
+        elif code == LIST:
+            data = fs.list_files(filename)
     conn.sendall(data)
     conn.close()
